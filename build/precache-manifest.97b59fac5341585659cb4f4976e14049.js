e0d66548b21efd9284c3d66ac1b560efb0e44fe8
Nishat Aziz <nishataziz09@gmail.com>
2025-05-21 16:03:20 -0400

    update

---
+++ build/precache-manifest.97b59fac5341585659cb4f4976e14049.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6469290acebbd5a91c2df822d5b395",
+    "url": "/To-do-list-with-react/index.html"
+  },
+  {
+    "revision": "c672cbffe43c0b1fc9f0",
+    "url": "/To-do-list-with-react/static/css/main.397d4e6b.chunk.css"
+  },
+  {
+    "revision": "db719f990d2b957a6c46",
+    "url": "/To-do-list-with-react/static/js/2.671cf31a.chunk.js"
+  },
+  {
+    "revision": "c672cbffe43c0b1fc9f0",
+    "url": "/To-do-list-with-react/static/js/main.a1b2a0d1.chunk.js"
+  },
+  {
+    "revision": "0c490a8864d6cdae49d6",
+    "url": "/To-do-list-with-react/static/js/runtime-main.71f6fb15.js"
+  }
+]);
--- src/components/todoItems.jsx
+++ src/components/todoItems.jsx
@@ -1,45 +1,41 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
+function TodoItems(props) {
+  const [isDone, setIsDone] = useState(false);
 
+  function handleClick() {
+    setIsDone((prevValue) => !prevValue);
+  }
 
-
-function TodoItems(props){
-
-    const [isDone,setIsDone]=useState(false)
-
-    function handleClick(){
-        setIsDone((prevValue)=>{
-            return !isDone
-        })
-    }
-
-    return(
-        <li 
-        onClick={handleClick}
-
-        
-        key={props.index}
-        style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            textDecoration: isDone? "line-through": "none"
-        }}
+  return (
+    <li
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
     >
+      <span
+        onClick={handleClick}
+        style={{
+          flex: 1,
+          cursor: "pointer",
+          textDecoration: isDone ? "line-through" : "none",
+        }}
+      >
         {props.item}
+      </span>
 
-        <button
-        onClick={(e) => {
-          e.stopPropagation();
+      <button
+        onClick={(event) => {
+          event.stopPropagation(); 
           props.deleteItem(props.index);
         }}
       >
         Delete
       </button>
     </li>
-    )
+  );
 }
-
-
 
 export default TodoItems;
